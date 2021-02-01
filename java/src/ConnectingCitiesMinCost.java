b94c9b52b3d27444fa9d93d95b90b59b0ede0432
it-tmx <IT@tmxintermodal.com>
2021-02-01 15:31:35 -0500

    interview solution

--- java/src/ConnectingCitiesMinCost.java
+++ java/src/ConnectingCitiesMinCost.java
@@ -1,6 +1,8 @@
 import java.util.*;
 /*
 https://leetcode.com/problems/connecting-cities-with-minimum-cost/
+
+minimum spanning tree ( mst )
 prim algo
 Time:
    O(ElgV);
@@ -11,18 +13,18 @@ Space:
 public class ConnectingCitiesMinCost {
 
     static class Edge  {
-        int dest;
-        int weight;
-        Edge(int dest, int weight){
-            this.dest = dest;
-            this.weight = weight;
+        int dst;
+        int wgt;
+        Edge(int dst, int wgt){
+            this.dst = dst;
+            this.wgt = wgt;
         }
     }
 
     public int minimumCost(int N, int[][] connections) {
         Map<Integer, List<Edge>> graph = new HashMap<>();
 //        Queue<Edge> queue = new PriorityQueue<>( (a,b)-> a.weight-b.weight);
-        Queue<Edge> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.weight));
+        Queue<Edge> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.wgt));
         for ( int[] edge : connections ){
             graph.computeIfAbsent(edge[0], (key)->new ArrayList<>() ).add( new Edge(edge[1], edge[2]));
             graph.computeIfAbsent(edge[1], (key)->new ArrayList<>() ).add( new Edge(edge[0], edge[2]));
@@ -37,13 +39,13 @@ public class ConnectingCitiesMinCost {
         queue.addAll(graph.getOrDefault(src, Collections.emptyList()));
 
         while( !queue.isEmpty() ){
-            Edge edge = queue.remove();
-            if ( visited.contains(edge.dest) ) continue;
-            visited.add(edge.dest);
-            total += edge.weight;
+            Edge edge = queue.poll();
+            if ( visited.contains(edge.dst) ) continue;
+            visited.add(edge.dst);
+            total += edge.wgt;
             // double check, saw a performance win
-            for ( Edge neighbor : graph.getOrDefault(edge.dest, Collections.emptyList()) ){
-                if ( !visited.contains(neighbor.dest) ) queue.add(neighbor);
+            for ( Edge nei : graph.getOrDefault(edge.dst, Collections.emptyList()) ){
+                if ( !visited.contains(nei.dst) ) queue.add(nei);
             }
         }
         return (visited.size() == graph.size()) ? total : -1;
