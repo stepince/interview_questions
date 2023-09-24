6c7ad7c81643ffc591ad36bd6a0827930b06b846
stepince <stephenpince@gmail.com>
2023-09-24 18:00:14 -0400

    Update Atoi.java

--- java/src/Atoi.java
+++ java/src/Atoi.java
@@ -6,44 +6,29 @@ https://leetcode.com/problems/string-to-integer-atoi/submissions/
 public class Atoi {
 
     public static int myAtoi(String str) {
-        int start = 0;
-        int base = 0;
-        int neg = 1;
-
-        char[] arry = str.toCharArray();
-
-        for ( ; start < arry.length; ++start ){
-            if (arry[start] == ' ') continue;
-            break;
-        }
-        if ( start == arry.length ) return 0;
-        if ( arry[start] == '-' ){
-            ++start;
-            neg=-1;
-        }
-        else if ( arry[start] == '+' ){
-            ++start;
-        }
-        if ( start == arry.length || arry[start] > '9' || arry[start] < '0' ) return 0;
-
-        int end = arry.length-1;
-        for ( ; end >= start; --end ){
-            if ( arry[end] > '9' || arry[end] < '0' ) continue;
-            break;
-        }
+        char[] chars = s.trim().toCharArray();
+        long base = 1;
         long val = 0;
-        for ( int i = end; i >= start; --i ){
-            if ( arry[i] > '9' || arry[i] < '0' ) {
-                base = 0;
-                val = 0;
-                continue;
-            }
-            int num = arry[i] - '0';
-            val += Math.pow(10,base++)*num;
-            if ( neg == 1 && val >= Integer.MAX_VALUE ) return Integer.MAX_VALUE;
-            else if ( neg == -1 && val >= Integer.MAX_VALUE + (long)1) return Integer.MIN_VALUE;
+        long sign = 1;
+        int start = 0;
+        if (start == chars.length) return 0;
+        if ( chars[start] == '-') {
+           ++start;
+           sign = -1;   
+        } else if ( chars[start] == '+' ) {
+           ++start;
         }
-        return neg*(int)val;
+        for(int i = chars.length -1; i >=start ; --i ) {
+           long num = chars[i] - '0';
+           if ( num >= 0 && num <= 9 ){
+              val += base * num;
+              base = Math.min(Integer.MAX_VALUE, base *= 10);
+           } else {
+               base = 1;
+               val = 0;
+           }
+        }
+        return sign == 1 ? (int)Math.min(Integer.MAX_VALUE,sign * val) : (int)Math.max(Integer.MIN_VALUE,sign * val);
     }
 
     public static void main(String[] args){
